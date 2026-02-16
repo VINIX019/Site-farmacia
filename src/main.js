9969ccc91569c65739829e21ee48db15d9e34ebe
Vinicius <vgaxavier@gmail.com>
2026-02-16 13:46:57 -0300

    atualizando

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
 import './style.css'
-
-const btn_wpp = document.getElementById('botao_wpp');
-
-btn_wpp.addEventListener('click', () => {
-  window.open(`https://wa.me/5519989776179`, '_blank');
-});
